46d8f5ce37cdb0f15bb8151a18d72580743fe551
Ben Haldenby <benhaldenby@gmail.com>
2023-02-21 10:51:27 +0000

    Remove refs to /dev/disk8s1 as they change

--- plex-reset.sh
+++ plex-reset.sh
@@ -3,25 +3,36 @@
 echo "Stopping Plex Media Server"
 osascript -e 'quit app "Plex Media Server"'
 sleep 3
+
 echo "Stopping Transmission"
 osascript -e 'quit app "Transmission"'
 sleep 3
+
 rm ~/Library/Application\ Support/Plex\ Media\ Server
-echo "Unmounting RAID"
-diskutil unmount /dev/disk8s1
-sleep 2
-echo "Mounting RAID"
-diskutil mount /dev/disk8s1
-sleep 6
+
+# echo "Unmounting RAID"
+# diskutil unmount /Volumes/RAID
+# diskutil unmount /Volumes/RAID
+# sleep 2
+
+# echo "Mounting RAID""
+# diskutil mount /dev/disk8s1
+# sleep 6
+
 echo "Renaming RAID"
-diskutil rename /dev/disk8s1 RAID
+diskutil rename /Volumes/RAID RAID
+diskutil rename /Volumes/RAID\ 1 RAID
 sleep 1
+
 echo "Creating symlink to RAID"
 ln -s /Volumes/RAID/Plex\ Media\ Server ~/Library/Application\ Support
 sleep 10
+
 open /Applications/Plex\ Media\ Server.app
 echo "Plex Media Server has been restarted"
+
 open /Applications/Transmission.app
 echo "Transmission has been restarted"
+
 echo "Update PlexCron.txt"
 touch ~/Library/Application\ Support/Plex\ Media\ Server/PlexReset.txt
